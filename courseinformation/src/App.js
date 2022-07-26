d8b44ac0dd035797820b3dc23cfd0c008714c92a
Mohamad EL-Chanti <m.elchanti@gmail.com>
2022-07-26 14:13:03 -0400

    Finished 1b and its exercises

--- courseinformation/src/App.js
+++ courseinformation/src/App.js
@@ -7,39 +7,48 @@ const Header = (props) => {
 const Part = (props) => {
   return (
     <p>
-      {props.name} {props.exercises}
+      {props.part.name} {props.part.exercises}
     </p>
   )
 }
 const Content = (props) => {
   return (
     <div>
-      <Part name = {props.part1} exercises={props.exercises1}/>
-      <Part name = {props.part2} exercises={props.exercises2}/>
-      <Part name = {props.part3} exercises={props.exercises3}/>
+      <Part part = {props.parts[0]}/>
+      <Part part = {props.parts[1]}/>
+      <Part part = {props.parts[2]}/>
     </div>
   )
 }
 
 const Total = (props) => {
   return (
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    <p>Number of exercises {props.parts[0].exercises + props.parts[0].exercises + props.parts[0].exercises}</p>
   )
 }
 const App = () => {
-  const course = 'Half Stack application develpment'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  const course = {
+    name: 'Half Stack application develpment',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
-      <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Header course={course.name} />
+      <Content parts = {course.parts}/>
+      <Total parts = {course.parts}/>
     </div>
   )
 }
---
+++ part1/javascript.md
@@ -0,0 +1,11 @@
+***Arrays***
+In React code, it's preferable to use the method `concat` to add an item into an array and create a new array in which the content of the old array and the new item are both inclided. Instead of using `push`.
+
+REACT uses the `map` method of arrays ofen
+
+***Object methods and "this"***
+`this` for methods and functions in JS behaves in odd ways.
+I know this because I took JS core and there it's explained pretty well.
+If you need a review, go to the OOP course of the launch school core and look at the material there.
+
+***Classes***
